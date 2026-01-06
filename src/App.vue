aee8d6c4faf481b4c93e482bd5b068d04dc273b7
temmur <143727496+temmur@users.noreply.github.com>
2026-01-06 10:08:26 +0500

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,57 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<!--<script setup>-->
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+<!--import TheWelcome from './components/TheWelcome.vue'-->
+<!--</script>-->
+
+<!--<template>-->
+<!--  <header>-->
+<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
+
+<!--    <div class="wrapper">-->
+<!--      <HelloWorld msg="You did it!" />-->
+<!--    </div>-->
+<!--  </header>-->
+
+<!--  <main>-->
+<!--    <TheWelcome />-->
+<!--  </main>-->
+<!--</template>-->
+
+<!--<style scoped>-->
+<!--header {-->
+<!--  line-height: 1.5;-->
+<!--}-->
+
+<!--.logo {-->
+<!--  display: block;-->
+<!--  margin: 0 auto 2rem;-->
+<!--}-->
+
+<!--@media (min-width: 1024px) {-->
+<!--  header {-->
+<!--    display: flex;-->
+<!--    place-items: center;-->
+<!--    padding-right: calc(var(&#45;&#45;section-gap) / 2);-->
+<!--  }-->
+
+<!--  .logo {-->
+<!--    margin: 0 2rem 0 0;-->
+<!--  }-->
+
+<!--  header .wrapper {-->
+<!--    display: flex;-->
+<!--    place-items: flex-start;-->
+<!--    flex-wrap: wrap;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
+
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1 class="text-red-600">Hello world</h1>
+    <p class="text-xl font-bold text-violet-500">Hello dev</p>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script setup>
+</script>
